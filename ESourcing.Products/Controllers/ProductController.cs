f29d191aa79d34d4f0594697c7a7265eb25fe86c
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-05-28 19:41:24 +0300

    First commit

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ESourcing.Products/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+//Monggodb data annotations anlayabilmesi adına yüklendi.
+namespace ESourcing.Products.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] //Idnin uniq olmasını sağlar. 
+        public string Id { get; set; } //Mongoda id Bson ID olarak gösterebilmek adına string olarak tanımladık.
+        [BsonElement("Name")] //display özelliği-Belirtilen şekilde görünmesini sağlayacak
+        public string Name { get; set; }
+        public string Category  { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
