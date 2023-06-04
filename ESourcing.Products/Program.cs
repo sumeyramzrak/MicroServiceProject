3d9f48e5aa780afd70ea86488e2a69e45348036b
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-06-04 20:43:24 +0300

    Add mongoDb configurations

--- ESourcing.Products/Program.cs
+++ ESourcing.Products/Program.cs
@@ -1,9 +1,16 @@
+using ESourcing.Products.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<ProductDatabaseSettings>(builder.Configuration.GetSection(nameof(ProductDatabaseSettings)));
+builder.Services.AddSingleton<IProductDatabaseSettings>(sp=>sp.GetRequiredService<IOptions<IProductDatabaseSettings>>().Value);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
