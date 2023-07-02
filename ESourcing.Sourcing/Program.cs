8ed8030f40dc44e49b4b5bbf24ac39925be55005
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-07-02 15:55:15 +0300

    Swagger implementation

--- ESourcing.Sourcing/Program.cs
+++ ESourcing.Sourcing/Program.cs
@@ -3,7 +3,7 @@ using ESourcing.Sourcing.Repositories;
 using ESourcing.Sourcing.Repositories.Interfaces;
 using ESourcing.Sourcing.Settings;
 using Microsoft.Extensions.Options;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,16 +11,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.Configure<SourcingDatabaseSettings>(builder.Configuration.GetSection(nameof(SourcingDatabaseSettings)));
-builder.Services.AddSingleton<ISourcingDatabaseSettings>(sp =>sp.GetRequiredService<IOptions<SourcingDatabaseSettings>>().Value);
+builder.Services.AddSingleton<ISourcingDatabaseSettings>(sp => sp.GetRequiredService<IOptions<SourcingDatabaseSettings>>().Value);
+
+#region Project Dependencies
 builder.Services.AddTransient<IAuctionRepository, AuctionRepository>();
 builder.Services.AddTransient<IBidRepository, BidRepository>();
+builder.Services.AddTransient<ISourcingContext, SourcingContext>();
+
+#endregion
+#region Swagger Dependencies
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo { Title = "ESourcing.Sourcing" , Version = "v1"});
+});
+#endregion
 
 #region ProjectDependencies
-builder.Services.AddTransient<ISourcingContext, SourcingContext>(); 
+builder.Services.AddTransient<ISourcingContext, SourcingContext>();
 #endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
