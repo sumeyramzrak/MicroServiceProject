44e723a2f6a1e56f572747cac658b386efa90ae9
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-07-02 15:31:41 +0300

    Presentation layer added

--- ESourcing.Sourcing/Controllers/AuctionController.cs
+++ ESourcing.Sourcing/Controllers/AuctionController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ESourcing.Sourcing.Entities;
+using ESourcing.Sourcing.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ESourcing.Sourcing.Controllers
 {
@@ -7,5 +10,66 @@ namespace ESourcing.Sourcing.Controllers
     [ApiController]
     public class AuctionController : ControllerBase //Controllerlar herhangi bir view dönmeyeceğinden ControllerBase i miras alıyor. View dönecek olsaydı Controller dan miras alırdı.
     {
+        private readonly IAuctionRepository _auctionRepository;
+        private readonly IBidRepository _bidRepository;
+        private readonly ILogger<AuctionController> _logger;
+
+        public AuctionController(
+            IAuctionRepository auctionRepository,
+            IBidRepository bidRepository,
+            ILogger<AuctionController> logger)
+        {
+            _auctionRepository = auctionRepository;
+            _bidRepository = bidRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Auction>>> GetAuctions()
+        {
+            var auctions = await _auctionRepository.GetAuctions();
+
+            return Ok(auctions);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetAuction")]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Auction>> GetAuction(string id)
+        {
+            var auction = await _auctionRepository.GetAuction(id);
+
+            if (auction == null)
+            {
+                _logger.LogError($"Auction with id: {id}, hasn't been found in database.");
+                return NotFound();
+            }
+
+            return Ok(auction);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Auction>> CreateAuction([FromBody] Auction auction)
+        {
+            await _auctionRepository.Create(auction);
+
+            return CreatedAtRoute("GetAuction", new { id = auction.Id }, auction); //GetAuction metoduna tekrar geri döndürerek kaydetme işleminin kontrolünü sağlamış olduk.
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> UpdateAuction([FromBody] Auction auction)
+        {
+            return Ok(await _auctionRepository.Update(auction));
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> DeleteAuctionById(string id)
+        {
+            return Ok(await _auctionRepository.Delete(id));
+        }
     }
 }
--- ESourcing.Sourcing/Controllers/BidController.cs
+++ ESourcing.Sourcing/Controllers/BidController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ESourcing.Sourcing.Entities;
+using ESourcing.Sourcing.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ESourcing.Sourcing.Controllers
 {
@@ -7,5 +10,48 @@ namespace ESourcing.Sourcing.Controllers
     [ApiController]
     public class BidController : ControllerBase
     {
+        private readonly IBidRepository _bidRepository;
+
+        public BidController(IBidRepository bidRepository)
+        {
+            _bidRepository = bidRepository;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> SendBid([FromBody] Bid bid)
+        {
+            await _bidRepository.SendBid(bid);
+
+            return Ok();
+        }
+
+        [HttpGet("GetBidByAuctionId")]
+        [ProducesResponseType(typeof(IEnumerable<Bid>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBidByAuctionId(string id)
+        {
+            IEnumerable<Bid> bids = await _bidRepository.GetBidsByAuctionId(id);
+
+            return Ok(bids);
+        }
+
+        [HttpGet("GetAllBidsByAuctionId")]
+        [ProducesResponseType(typeof(List<Bid>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetAllBidsByAuctionId(string id)
+        {
+            IEnumerable<Bid> bids = await _bidRepository.GetAllBidsByAuctionId(id);
+
+            return Ok(bids);
+        }
+
+        [HttpGet("GetWinnerBid")]
+        [ProducesResponseType(typeof(Bid), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Bid>> GetWinnerBid(string id)
+        {
+            Bid bid = await _bidRepository.GetWinnerBid(id);
+
+            return Ok(bid);
+        }
+
     }
 }
