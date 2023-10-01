af0dea135c9f3c6b0dcb45bd61d2bf36b1592288
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-10-01 15:00:22 +0300

    Web app project created and layout files added

---
+++ ESourcing.UI/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Certificate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddMvc();
+var app = builder.Build();
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddAuthentication(
+        CertificateAuthenticationDefaults.AuthenticationScheme)
+        .AddCertificate();
+
+    // All other service configuration
+}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(name: "Default", pattern: "{controller=Home}/{action=index}/{id?}");
+    endpoints.MapRazorPages();
+});
+app.Run();
