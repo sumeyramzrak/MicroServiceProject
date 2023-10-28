a0524727aec9939a8df8fd3344fc482d5a75e743
SÜMEYRA MIZRAK <sumeyra.mizrak@gmail.com>
2023-10-28 17:01:58 +0300

    Added auction index,create and details pages

--- ESourcing.UI/Controllers/AuctionController.cs
+++ ESourcing.UI/Controllers/AuctionController.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ESourcing.UI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ESourcing.UI.Controllers
 {
     public class AuctionController : Controller
     {
+
         public IActionResult Index()
+        {
+            List<AuctionViewModel> model = new List<AuctionViewModel>();
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AuctionViewModel model)
+        {
+            return View(model);
+        }
+
+        public async Task<IActionResult> Detail(string id)
         {
             return View();
         }
